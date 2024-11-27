c97246323a7ee4733e2fa3deed46d475d02325e7
Umar Bhaiyat <bhaiyatu@icloud.com>
2024-11-27 22:26:23 +0000

    Initial Commit

---
+++ RandomApp/RandomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomAppApp.swift
+//  RandomApp
+//
+//  Created by Umar Bhaiyat on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RandomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomAppTests/RandomAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomAppTests.swift
+//  RandomAppTests
+//
+//  Created by Umar Bhaiyat on 27/11/2024.
+//
+
+import Testing
+@testable import RandomApp
+
+struct RandomAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
